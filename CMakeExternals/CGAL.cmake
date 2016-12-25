20392cbc53759cc96f9d40b9e7ff6ce349ec80a4
Araex <araex@araex.ch>
2016-12-25 16:39:47 +0100

    Added CGAL as an external package

---
+++ CMakeExternals/CGAL.cmake
@@ -0,0 +1,56 @@
+#-----------------------------------------------------------------------------
+# CGAL
+#-----------------------------------------------------------------------------
+
+if(MITK_USE_CGAL)
+
+  # Sanity checks
+  if(DEFINED CGAL_DIR AND NOT EXISTS ${CGAL_DIR})
+    message(FATAL_ERROR "CGAL_DIR variable is defined but corresponds to non-existing directory")
+  endif()
+
+  set(proj CGAL)
+  set(proj_DEPENDENCIES Boost VTK)
+  set(CGAL_DEPENDS ${proj})
+
+  set(preconfigure_cmake_args )
+
+  if(NOT MITK_USE_SYSTEM_Boost)
+    list(APPEND preconfigure_cmake_args
+      -DBoost_NO_SYSTEM_PATHS:BOOL=ON
+      -DBOOST_ROOT:PATH=${BOOST_ROOT}
+      -DBOOST_LIBRARYDIR:PATH=${BOOST_LIBRARYDIR}
+      -DBoost_ADDITIONAL_VERSIONS:STRING=1.59${sep}1.59.0
+    )
+  endif()
+
+  if(NOT DEFINED CGAL_DIR)
+    set(additional_cmake_args)
+
+    ExternalProject_Add(${proj}
+      LIST_SEPARATOR ${sep}
+
+      URL "https://github.com/CGAL/cgal/releases/download/releases%2FCGAL-4.9/CGAL-4.9.tar.xz"
+
+      CMAKE_GENERATOR ${gen}
+
+      CMAKE_ARGS
+        ${ep_common_args}
+        ${preconfigure_cmake_args}
+        # -DBUILD_EXAMPLES:BOOL=OFF
+
+      CMAKE_CACHE_ARGS
+        ${ep_common_cache_args}
+
+      CMAKE_CACHE_DEFAULT_ARGS
+        ${ep_common_cache_default_args}
+
+      DEPENDS ${proj_DEPENDENCIES}
+    )
+
+    set(CGAL_DIR ${ep_prefix})
+    mitkFunctionInstallExternalCMakeProject(${proj})
+  else()
+    mitkMacroEmptyExternalProject(${proj} "${proj_DEPENDENCIES}")
+  endif()
+endif()
